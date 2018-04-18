1d637d340d74fa80c74826949e0c7744177ceb30
Jay Karimi <jkarimi91@gmail.com>
2018-04-18 15:29:13 -0700

    Refactoring

--- app/utils.py
+++ app/utils.py
@@ -20,10 +20,13 @@ def preprocess(img, target_size):
             half_height + half_min_dim
         )
     )
+
     # Greyscale and resize
     img = img.convert('L').resize(target_size)
-    # Feature scale
+
+    # Feature scale image using the same values used at training time
     img = (img_to_array(img) / 255. - .5) * 2.
+
     # Add batch dim i.e. model expects arrays of shape (batch, height, width, channels)
     return np.expand_dims(img, 0)
 
