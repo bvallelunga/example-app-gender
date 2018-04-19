07ada8754f37382b0bf4031cd43fd5609054f83e
Jay Karimi <jkarimi91@gmail.com>
2018-04-18 23:43:26 -0700

    Changing input key from 'face' to 'image'

--- app/main.py
+++ app/main.py
@@ -19,16 +19,16 @@ class ModelInterface(object):
         self.model = model
 
     def predict(self, input):
-        if 'face' not in input:
-            raise KeyError("No key named 'face' in input.")
+        if 'image' not in input:
+            raise KeyError("No key named 'image' in input.")
         try:
-            face = base64_to_img(input['face'])
+            img = base64_to_img(input['image'])
         except (binascii.Error, AttributeError):
-            raise ValueError("'face' must be a base64 encoded string.")
+            raise ValueError("'image' must be a base64 encoded string.")
         min_dim = self.model.input_shape[1]
-        if any(dim < min_dim for dim in face.size):
-            raise ValueError("'face' can not have a height or width less than {} pixels.".format(min_dim))
+        if any(dim < min_dim for dim in img.size):
+            raise ValueError("'image' can not have a height or width less than {} pixels.".format(min_dim))
 
-        face = preprocess(face, self.model.input_shape[1:3])
-        scores = self.model.predict(face).tolist()[0]
+        img = preprocess(img, self.model.input_shape[1:3])
+        scores = self.model.predict(img).tolist()[0]
         return {'scores': {label: round(score, SCORE_PRECISION) for label, score in zip(LABELS, scores)}}
--- test/test_model_interface.py
+++ test/test_model_interface.py
@@ -22,41 +22,41 @@ class TestModelInterface(unittest.TestCase):
 
     def test_output_has_scores_key(self):
         """The output dict must have a 'scores' key."""
-        result = self.interface.predict({'face': IMG})
+        result = self.interface.predict({'image': IMG})
         self.assertIn('scores', result)
 
     def test_scores_has_woman_key(self):
         """The scores dict must have a 'woman' key."""
-        scores = self.interface.predict({'face': IMG})['scores']
+        scores = self.interface.predict({'image': IMG})['scores']
         self.assertIn('woman', scores)
 
     def test_scores_has_man_key(self):
         """The scores dict must have a 'man' key."""
-        scores = self.interface.predict({'face': IMG})['scores']
+        scores = self.interface.predict({'image': IMG})['scores']
         self.assertIn('man', scores)
 
     def test_scores_are_floats(self):
         """The score values must be floats."""
-        scores = self.interface.predict({'face': IMG})['scores']
+        scores = self.interface.predict({'image': IMG})['scores']
         self.assertTrue(all(isinstance(score, float) for score in scores.values()))
 
     def test_scores_have_correct_precision(self):
         """The scores must have the correct precision."""
-        scores = self.interface.predict({'face': IMG})['scores']
+        scores = self.interface.predict({'image': IMG})['scores']
         self.assertTrue(all(score == round(score, SCORE_PRECISION) for score in scores.values()))
 
-    def test_face_is_not_a_string(self):
-        """'face' must be a string."""
+    def test_image_is_not_a_string(self):
+        """'image' must be a string."""
         with self.assertRaises(ValueError):
-            self.interface.predict({'face': []})
+            self.interface.predict({'image': []})
 
-    def test_face_is_not_base64_encoded(self):
-        """'face' must be a base64 encoded string."""
+    def test_image_is_not_base64_encoded(self):
+        """'image' must be a base64 encoded string."""
         with self.assertRaises(ValueError):
-            self.interface.predict({'face': '<'})
+            self.interface.predict({'image': '<'})
 
-    def test_face_is_too_small(self):
-        """'face' can not be smaller than the model's expected input size."""
+    def test_image_is_too_small(self):
+        """'image' can not be smaller than the model's expected input size."""
         with self.assertRaises(ValueError):
             img = img_to_base64(os.path.join(BASE_PATH, 'test_small.jpg'))
-            self.interface.predict({'face': img})
+            self.interface.predict({'image': img})
